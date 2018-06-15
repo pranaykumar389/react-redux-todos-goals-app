8509a3566fdcc0b75ffd0c6319c079c8bdd00ada
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2018-06-14 23:03:00 -0400

    Reducer Function

--- store.js
+++ store.js
@@ -1,3 +1,12 @@
+//Reducer Function
+function todos(state = [], action) {
+    if (action.type === 'ADD_TODO') {
+        return state.concat([action.todo]);
+    }
+
+    return state;
+}
+
 function createStore() {
     let state;
     let listeners = [];
